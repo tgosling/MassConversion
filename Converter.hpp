cb866c386efdd4cb1c4de2af17d755c2c02922e5
Tyler Gosling <t.gosling61@gmail.com>
2019-07-09 17:47:25 -0400

    finished all conversions, handled double input

--- Converter.hpp
+++ Converter.hpp
@@ -14,29 +14,42 @@ pair<double, string> convertMass(pair<double, string> usrMass)
 {
 	pair<double, string> convMass;
 
-	if (usrMass.second == "grams")
+	if (usrMass.second == "grams" || usrMass.second == "g")
 	{
 		convMass.first = usrMass.first * 0.035724;
 		convMass.second = "oz";
 	}
-	else if (usrMass.second == "oz")
+	else if (usrMass.second == "oz" || usrMass.second == "ounces")
 	{
 		convMass.first = usrMass.first * 28.3495;
 		convMass.second = "grams";
 	}
-	else if (usrMass.second == "lb")
+	else if (usrMass.second == "lb" || usrMass.second == "pounds")
 	{
 		convMass.first = usrMass.first * 0.453592;
 		convMass.second = "kg";
 	}
-	else if (usrMass.second == "kg")
+	else if (usrMass.second == "kg" || usrMass.second == "kilograms")
 	{
 		convMass.first = usrMass.first * 2.20462;
 		convMass.second = "lb";
 	}
+	else if (usrMass.second == "gr" || usrMass.second == "grain")
+	{
+		convMass.first = usrMass.first * 0.0416667;
+		convMass.second = "dwt";
+	}
+	else if (usrMass.second == "dwt" || usrMass.second == "pennyweight")
+	{
+		convMass.first = usrMass.first * 24;
+		convMass.second = "gr";
+	}
 	else
 	{
 		std::cout << "ERROR! not a recognized mass type";
+		convMass.first = 0;
+		convMass.second = "Error";
+		return convMass;
 	}
 
 	return convMass;
--- MassConversion.cpp
+++ MassConversion.cpp
@@ -2,6 +2,7 @@
 //
 #include "Converter.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,16 +13,47 @@ int main()
 	pair<double, string> usrMass;
 	pair<double, string> convMass;
 	string contConv = "y";
+	double dblInput;
+	string strInput;
 
 	cout << "\tMass Conversion Application" << endl;
 	cout << "\t---------------------------" << endl;
 	while (contConv == "y") {
 		//prompt for user info
 		cout << "\nEnter mass and mass type: ";
-		cin >> usrMass.first >> usrMass.second;
-		
+		//cin >> dblInput >> strInput;
+		do {
+			if (cin >> dblInput) {
+				break;
+			}
+			else {
+				cout << "enter a number in digits or larger than 0" << endl;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			}
+		} while (cin.fail() || dblInput == 0);
+		/*while (1) {
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout << "enter a number in digits" << endl;
+				cin >> dblInput;
+			}
+			if (!cin.fail())
+				break;
+		}*/
+		cin >> strInput;
+		strInput.erase(remove_if(strInput.begin(), strInput.end(), ::isspace), strInput.end());
+		usrMass.first = dblInput;
+		usrMass.second = strInput;
+		//Clean up mass/type user input
+		usrMass.second.erase(remove_if(usrMass.second.begin(), usrMass.second.end(), ::isspace), usrMass.second.end());
+		transform(usrMass.second.begin(), usrMass.second.end(), usrMass.second.begin(), ::tolower);
 		cout << "\n-------------------------" << endl;
+		//convert data
 		convMass = convertMass(usrMass);
+		//display conversion
 		cout << "\nUser Mass: " << usrMass.first << " " << usrMass.second << endl;
 		cout << "Converted Mass: " << convMass.first << " " << convMass.second << endl;
 		cout << "\nConvert another value? (y/n)";
